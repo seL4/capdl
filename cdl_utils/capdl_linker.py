1dfcb85d57d4752329c48ed5676fa2ae90fd2d67
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2018-10-15 14:30:52 +1100

    Add cdl_utils module for non Camkes apps
    This module is currently used by the tutorials to bootstrap some simple
    processes. Its interfaces are currently very experimental.
    
    Has two main modes:
    - build_cnode:
        Will generate a c file for building into an application that defines
    symbols for naming CSpace slots. Additionally defines an IPC buffer and
    special address space regions.
    - gen_cdl:
        Will take a list of elf files and build the final capDL spec for
    loading them.

---
+++ cdl_utils/capdl_linker.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+#
+# Copyright 201*, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+import sys
+import argparse
+import pickle
+import logging
+import os
+import tempfile
+import pkg_resources
+import six
+pkg_resources.require("jinja2>=2.10")
+from jinja2 import Environment, BaseLoader, FileSystemLoader
+
+from capdl import ELF, lookup_architecture
+
+CSPACE_TEMPLATE_FILE = os.path.join(os.path.dirname(__file__), "templates/cspace.template.c")
+
+
+def manifest(cap_symbols, region_symbols, architecture, targets):
+    """
+    Generates a c file from CSPACE_TEMPLATE_FILE with some runtime information
+    about CSpace slots and special address ranges
+    """
+    temp_file = open(CSPACE_TEMPLATE_FILE, 'r').read()
+    template = Environment(loader=BaseLoader).from_string(temp_file)
+
+    for (e, ccspace) in targets:
+        name = os.path.basename(e)
+        if ccspace:
+            data = template.render({'slots': cap_symbols[name], 'symbols': region_symbols[name], 'progname': name, 'ipc_buffer_symbol': "mainIpcBuffer"})
+            ccspace.write(data)
+
+def final_spec(cspaces, obj_space, addr_spaces, elf_files, architecture):
+    """
+    Generates a final CapDL spec file that can be given to a capdl loader application
+    """
+    arch = lookup_architecture(architecture)
+
+    for e in [item for sublist in elf_files for item in sublist]:
+        name = os.path.basename(e)
+        elf = ELF(e, name, architecture)
+        cspace = cspaces[name]
+
+        # Avoid inferring a TCB as we've already created our own.
+        elf_spec = elf.get_spec(infer_tcb=False, infer_asid=False,pd=addr_spaces[name].vspace_root, addr_space=addr_spaces[name])
+        obj_space.merge(elf_spec)
+        cspace.cnode.finalise_size(arch)
+
+        # Fill in TCB object information.
+        # TODO: This should be generalised with what is in the Camkes filters
+        tcb = obj_space["tcb_%s" % name]
+        progsymbol = elf.get_symbol_vaddr("progname")
+        vsyscall = elf.get_symbol_vaddr("sel4_vsyscall")
+        tcb.init = [0,0,0,0,2,progsymbol,1,0,0,32,vsyscall,0,0]
+        tcb.addr = elf.get_symbol_vaddr("mainIpcBuffer");
+        tcb.sp = elf.get_symbol_vaddr("stack")+elf.get_symbol_size("stack");
+        tcb.ip = elf.get_entry_point()
+
+    return obj_space
+
+def main():
+    parser = argparse.ArgumentParser(
+                description="")
+    parser.add_argument('--architecture', '--arch', default='aarch32',
+        type=lambda x: type('')(x).lower(), choices=('aarch32', 'arm_hyp', 'ia32', 'x86_64'),
+        help='Target architecture.')
+    subparsers = parser.add_subparsers()
+    parser_a = subparsers.add_parser('build_cnode')
+    parser_a.add_argument('--ccspace', nargs='+', type=argparse.FileType('w'), action='append')
+    parser_a.set_defaults(which="build_cnode")
+    parser_a.add_argument('--manifest-in', type=argparse.FileType('rb'))
+    parser_a.add_argument('--elffile', nargs='+', action='append')
+    parser_b = subparsers.add_parser('gen_cdl')
+    parser_b.add_argument('--outfile', type=argparse.FileType('w'))
+    parser_b.set_defaults(which="gen_cdl")
+    parser_b.add_argument('--manifest-in', type=argparse.FileType('rb'))
+    parser_b.add_argument('--elffile', nargs='+', action='append')
+    args = parser.parse_args()
+
+    (objects, cspaces, addr_spaces, cap_symbols, region_symbols, elfs) = pickle.load(args.manifest_in)
+    if args.which is "build_cnode":
+        elfs = [item for sublist in args.elffile for item in sublist]
+        cspaces = [item for sublist in args.ccspace for item in sublist]
+        targets = zip(elfs, cspaces)
+        manifest(cap_symbols, region_symbols, args.architecture, targets)
+        return 0
+
+    if args.which is "gen_cdl":
+        obj_space = final_spec(cspaces, objects, addr_spaces, args.elffile, args.architecture)
+        args.outfile.write(repr(obj_space.spec))
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ cdl_utils/templates/cspace.template.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#include <sel4/sel4.h>
+#include <utils/util.h>
+
+{% raw %}
+#define DECLARE_IPCBUFFER_SYMBOL(symbol) \
+extern char symbol[]; \
+void CONSTRUCTOR(199) setUserData(void) { \
+	seL4_SetUserData((seL4_Word)symbol); \
+}
+{% endraw %}
+
+DECLARE_IPCBUFFER_SYMBOL({{ipc_buffer_symbol}})
+
+#define SIZED_SYMBOL(symbol, size, section) \
+	char symbol[size] VISIBLE ALIGN(4096) SECTION(section);
+
+{% for (symbol, slot) in slots -%}
+seL4_CPtr {{symbol}} = {{slot}};
+{% endfor %}
+
+{% for (symbol, size, section) in symbols -%}
+SIZED_SYMBOL({{symbol}}, {{size}}, "{{section}}")
+{% endfor %}
+
+
+char progname[] = "{{progname}}";
