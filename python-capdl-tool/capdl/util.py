fc884ff835287441c1a3f0fdc651ff8def1417f7
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-04-15 19:25:25 +1000

    trivial: python-capdl-tool: style util.py

--- python-capdl-tool/capdl/util.py
+++ python-capdl-tool/capdl/util.py
@@ -23,8 +23,8 @@ from six.moves import range
 from .Object import ObjectType, PageTable, PageDirectory, PML4, PDPT, PGD, PUD, get_object_size
 
 # Size of a frame and page (applies to all architectures)
-FRAME_SIZE = 4096 # bytes
-PAGE_SIZE = 4096 # bytes
+FRAME_SIZE = 4096  # bytes
+PAGE_SIZE = 4096  # bytes
 
 SIZE_64K = 64 * 1024
 SIZE_1M = 1024 * 1024
@@ -35,6 +35,7 @@ SIZE_32M = 32 * SIZE_1M
 SIZE_1GB = 1 * 1024 * 1024 * 1024
 SIZE_4GB = 4 * SIZE_1GB
 
+
 class Level:
     """
     Abstraction of a 'level' in a virtual address space hierarchy. A level
@@ -43,7 +44,8 @@ class Level:
     the level and what kinds of frames (if any) can be placed directly
     at this level.
     """
-    def __init__(self, coverage, pages, object, make_object, type_name, parent = None, child = None):
+
+    def __init__(self, coverage, pages, object, make_object, type_name, parent=None, child=None):
         self.coverage = coverage
         self.pages = pages
         self.parent = parent
@@ -51,18 +53,21 @@ class Level:
         self.object = object
         self.make_object = make_object
         self.type_name = type_name
+
     def base_vaddr(self, vaddr):
         """
         Base address of the range covered by this level, determined
         by using a virtual address from somewhere inside this object
         """
         return round_down(vaddr, self.coverage)
+
     def parent_index(self, vaddr):
         """
         Index of this object in the parent level. Determine by a
         using a virtual address from inside this object
         """
         return self.parent.child_index(vaddr)
+
     def child_index(self, vaddr):
         """
         Index of a child object that is contained in this object.
@@ -75,6 +80,7 @@ class Level:
         else:
             return vaddr % self.coverage // self.child.coverage
 
+
 def make_levels(levels):
     assert levels is not None
     assert len(levels) > 0
@@ -85,6 +91,7 @@ def make_levels(levels):
             levels[i].child = levels[i + 1]
     return levels[0]
 
+
 class Arch:
     def get_pages(self):
         level = self.vspace()
@@ -94,33 +101,47 @@ class Arch:
             level = level.child
         return pages
 
+
 class IA32Arch(Arch):
     def capdl_name(self):
         return "ia32"
+
     def vspace(self):
         return make_levels([
-            Level(SIZE_4GB, [ObjectType.seL4_LargePageObject], ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
-            Level(SIZE_4M, [ObjectType.seL4_SmallPageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(SIZE_4GB, [ObjectType.seL4_LargePageObject],
+                  ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
+            Level(SIZE_4M, [ObjectType.seL4_SmallPageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
         ])
+
     def word_size_bits(self):
         return 32
+
     def ipc_buffer_size(self):
         return 512
+
 
 class X64Arch(Arch):
     def capdl_name(self):
         return "x86_64"
+
     def vspace(self):
         return make_levels([
             Level(2 ** 48, [], ObjectType.seL4_X64_PML4, PML4, "pml4"),
-            Level(2 ** 39, [ObjectType.seL4_HugePageObject], ObjectType.seL4_X64_PDPT, PDPT, "pdpt"),
-            Level(2 ** 30, [ObjectType.seL4_LargePageObject], ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
-            Level(2 ** 21, [ObjectType.seL4_SmallPageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 39, [ObjectType.seL4_HugePageObject],
+                  ObjectType.seL4_X64_PDPT, PDPT, "pdpt"),
+            Level(2 ** 30, [ObjectType.seL4_LargePageObject],
+                  ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
+            Level(2 ** 21, [ObjectType.seL4_SmallPageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
         ])
+
     def word_size_bits(self):
         return 64
+
     def ipc_buffer_size(self):
         return 1024
+
 
 class ARM32Arch(Arch):
     def __init__(self, hyp=False):
@@ -128,69 +149,96 @@ class ARM32Arch(Arch):
 
     def capdl_name(self):
         return "arm11"
+
     def vspace(self):
         return make_levels([
-            Level(SIZE_4GB, [ObjectType.seL4_ARM_SectionObject, ObjectType.seL4_ARM_SuperSectionObject], ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
-            Level(SIZE_2M if self.hyp else SIZE_1M, [ObjectType.seL4_SmallPageObject, ObjectType.seL4_LargePageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(SIZE_4GB, [ObjectType.seL4_ARM_SectionObject, ObjectType.seL4_ARM_SuperSectionObject],
+                  ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
+            Level(SIZE_2M if self.hyp else SIZE_1M, [
+                  ObjectType.seL4_SmallPageObject, ObjectType.seL4_LargePageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
         ])
+
     def word_size_bits(self):
         return 32
+
     def ipc_buffer_size(self):
         return 512
+
 
 class AARCH64Arch(Arch):
     def capdl_name(self):
         return "aarch64"
+
     def vspace(self):
         return make_levels([
             Level(2 ** 48, [], ObjectType.seL4_AARCH64_PGD, PGD, "pgd"),
-            Level(2 ** 39, [ObjectType.seL4_HugePageObject], ObjectType.seL4_AARCH64_PUD, PUD, "pud"),
-            Level(2 ** 30, [ObjectType.seL4_LargePageObject], ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
-            Level(2 ** 21, [ObjectType.seL4_SmallPageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 39, [ObjectType.seL4_HugePageObject],
+                  ObjectType.seL4_AARCH64_PUD, PUD, "pud"),
+            Level(2 ** 30, [ObjectType.seL4_LargePageObject],
+                  ObjectType.seL4_PageDirectoryObject, PageDirectory, "pd"),
+            Level(2 ** 21, [ObjectType.seL4_SmallPageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
         ])
+
     def word_size_bits(self):
         return 64
+
     def ipc_buffer_size(self):
         return 1024
+
 
 class RISCV64Arch(Arch):
     def capdl_name(self):
         return "riscv"
+
     def vspace(self):
         return make_levels([
-            Level(2 ** 39, [ObjectType.seL4_HugePageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
-            Level(2 ** 30, [ObjectType.seL4_LargePageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
-            Level(2 ** 21, [ObjectType.seL4_SmallPageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 39, [ObjectType.seL4_HugePageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 30, [ObjectType.seL4_LargePageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 21, [ObjectType.seL4_SmallPageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
         ])
+
     def word_size_bits(self):
         return 64
+
     def ipc_buffer_size(self):
         return 1024
+
+
 class RISCV32Arch(Arch):
     def capdl_name(self):
         return "riscv"
+
     def vspace(self):
         return make_levels([
-            Level(2 ** 32, [ObjectType.seL4_LargePageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
-            Level(2 ** 22, [ObjectType.seL4_SmallPageObject], ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 32, [ObjectType.seL4_LargePageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
+            Level(2 ** 22, [ObjectType.seL4_SmallPageObject],
+                  ObjectType.seL4_PageTableObject, PageTable, "pt"),
         ])
+
     def word_size_bits(self):
         return 32
+
     def ipc_buffer_size(self):
         return 512
 
+
 def normalise_architecture(arch):
     normalise = {
-        'aarch32':'aarch32',
-        'aarch64':'aarch64',
-        'arm':'aarch32',
-        'arm11':'aarch32',
-        'arm_hyp':'arm_hyp',
-        'ia32':'ia32',
-        'x86':'ia32',
-        'x86_64':'x86_64',
-        'riscv64':'riscv64',
-        'riscv32':'riscv32'
+        'aarch32': 'aarch32',
+        'aarch64': 'aarch64',
+        'arm': 'aarch32',
+        'arm11': 'aarch32',
+        'arm_hyp': 'arm_hyp',
+        'ia32': 'ia32',
+        'x86': 'ia32',
+        'x86_64': 'x86_64',
+        'riscv64': 'riscv64',
+        'riscv32': 'riscv32'
     }
     try:
         return normalise[arch.lower()]
@@ -213,11 +261,13 @@ def lookup_architecture(arch):
     except KeyError:
         raise Exception('invalid architecture: %s' % arch)
 
+
 def round_down(n, alignment=FRAME_SIZE):
     """
     Round a number down to 'alignment'.
     """
     return n // alignment * alignment
+
 
 def round_up(n, alignment):
     """
@@ -225,21 +275,25 @@ def round_up(n, alignment):
     """
     return round_down(n + alignment-1, alignment)
 
+
 def last_level(level):
     while level.child is not None:
         level = level.child
     return level
+
 
 def page_sizes(arch):
     list = [get_object_size(page) for page in lookup_architecture(arch).get_pages()]
     list.sort()
     return list
 
+
 def page_table_coverage(arch):
     """
     The number of bytes a page table covers.
     """
     return last_level(lookup_architecture(arch).vspace()).coverage
+
 
 def page_table_vaddr(arch, vaddr):
     """
@@ -248,12 +302,14 @@ def page_table_vaddr(arch, vaddr):
     """
     return last_level(lookup_architecture(arch).vspace()).base_vaddr(vaddr)
 
+
 def page_table_index(arch, vaddr):
     """
     The index of a page table within a containing page directory, derived from
     the virtual address of a location within that table's coverage.
     """
     return last_level(lookup_architecture(arch).vspace()).parent_index(vaddr)
+
 
 def page_index(arch, vaddr):
     """
@@ -262,12 +318,14 @@ def page_index(arch, vaddr):
     """
     return last_level(lookup_architecture(arch).vspace()).child_index(vaddr)
 
+
 def page_vaddr(vaddr):
     """
     The base virtual address of a page, derived from the virtual address of a
     location within that page.
     """
     return vaddr // PAGE_SIZE * PAGE_SIZE
+
 
 def ctz(size_bytes):
     """
@@ -282,6 +340,7 @@ def ctz(size_bytes):
         low = low >> 1
         low_bit += 1
     return low_bit
+
 
 def is_aligned(value, size_bits):
     """
