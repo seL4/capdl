1a7b94503478c90175f61cf247671b5c5a7f1b8f
Yu Hou <Yu.Hou@data61.csiro.au>
2019-07-12 18:42:46 +1000

    Trivial: style fix
    make the style checker happy

--- capdl-loader-app/helpers.cmake
+++ capdl-loader-app/helpers.cmake
@@ -14,30 +14,50 @@ cmake_minimum_required(VERSION 3.7.2)
 
 function(BuildCapDLApplication)
     cmake_parse_arguments(PARSE_ARGV 0 CAPDL_BUILD_APP "" "C_SPEC;OUTPUT" "ELF;DEPENDS")
-    if (NOT "${CAPDL_BUILD_APP_UNPARSED_ARGUMENTS}" STREQUAL "")
+    if(NOT "${CAPDL_BUILD_APP_UNPARSED_ARGUMENTS}" STREQUAL "")
         message(FATAL_ERROR "Unknown arguments to BuildCapDLApplication")
     endif()
     # Require a cspec and an output
-    if ("${CAPDL_BUILD_APP_C_SPEC}" STREQUAL "")
+    if("${CAPDL_BUILD_APP_C_SPEC}" STREQUAL "")
         message(FATAL_ERROR "C_SPEC is required argument to BuildCapDLApplication")
     endif()
-    if ("${CAPDL_BUILD_APP_OUTPUT}" STREQUAL "")
+    if("${CAPDL_BUILD_APP_OUTPUT}" STREQUAL "")
         message(FATAL_ERROR "OUTPUT is required argument to BuildCapDLApplication")
     endif()
     # Build a CPIO archive out of the provided ELF files
-    MakeCPIO(${CAPDL_BUILD_APP_OUTPUT}_archive.o "${CAPDL_BUILD_APP_ELF}"
-        CPIO_SYMBOL _capdl_archive
+    MakeCPIO(
+        ${CAPDL_BUILD_APP_OUTPUT}_archive.o
+        "${CAPDL_BUILD_APP_ELF}"
+        CPIO_SYMBOL
+        _capdl_archive
     )
     # Build the application
-    add_executable("${CAPDL_BUILD_APP_OUTPUT}" EXCLUDE_FROM_ALL
+    add_executable(
+        "${CAPDL_BUILD_APP_OUTPUT}"
+        EXCLUDE_FROM_ALL
         $<TARGET_PROPERTY:capdl_app_properties,C_FILES>
         ${CAPDL_LOADER_APP_C_FILES}
         ${CAPDL_BUILD_APP_OUTPUT}_archive.o
         ${CAPDL_BUILD_APP_C_SPEC}
     )
     add_dependencies("${CAPDL_BUILD_APP_OUTPUT}" ${CAPDL_BUILD_APP_DEPENDS})
-    target_include_directories("${CAPDL_BUILD_APP_OUTPUT}" PRIVATE $<TARGET_PROPERTY:capdl_app_properties,INCLUDE_DIRS>)
-    target_link_libraries("${CAPDL_BUILD_APP_OUTPUT}" sel4runtime muslc sel4 elf cpio sel4platsupport sel4utils sel4muslcsys capdl_loader_app_Config kernel_autoconf)
+    target_include_directories(
+        "${CAPDL_BUILD_APP_OUTPUT}"
+        PRIVATE $<TARGET_PROPERTY:capdl_app_properties,INCLUDE_DIRS>
+    )
+    target_link_libraries(
+        "${CAPDL_BUILD_APP_OUTPUT}"
+        sel4runtime
+        muslc
+        sel4
+        elf
+        cpio
+        sel4platsupport
+        sel4utils
+        sel4muslcsys
+        capdl_loader_app_Config
+        kernel_autoconf
+    )
 endfunction(BuildCapDLApplication)
 
 # Hook for CAmkES build system. This allows CAmkES projects to
