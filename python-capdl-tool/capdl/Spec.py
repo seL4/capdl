12120668aea2cb8e5d3708c19388d929f7d89d03
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-03-20 11:31:50 +1100

    python-capdl-tool: support for domain schedules
    Add support for setting up domain schedules. By default, no schedule is
    added.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- python-capdl-tool/capdl/Spec.py
+++ python-capdl-tool/capdl/Spec.py
@@ -7,7 +7,7 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
-from .Object import IRQ, Object, PageTable
+from .Object import IRQ, Object
 from .util import lookup_architecture
 
 
@@ -19,6 +19,12 @@ class Spec(object):
     def __init__(self, arch='arm11'):
         self.arch = arch
         self.objs = set()
+        # empty list means no schedule will be set
+        self.schedule = []
+        # None means default (= 0), -1 means no domain_set_start will be performed
+        self.domain_set_start = None
+        # None means default (= 0)
+        self.index_shift = None
 
     @property
     def arch(self):
@@ -35,18 +41,46 @@ class Spec(object):
         assert isinstance(obj, Object)
         self.objs.add(obj)
 
+    def add_schedule_item(self, domain, duration):
+        self.schedule.append((domain, duration))
+
+    def add_schedule(self, schedule):
+        self.schedule.extend(schedule)
+
     def merge(self, other):
         assert isinstance(other, Spec)
         self.objs.update(other)
+        assert (not self.schedule or not other.schedule or self.schedule == other.schedule)
+        assert (self.domain_set_start is None or other.domain_set_start is None or
+                self.domain_set_start == other.domain_set_start)
+        assert (self.index_shift is None or other.index_shift is None or
+                self.index_shift == other.index_shift)
+        self.schedule = self.schedule or other.schedule
+        self.domain_set_start = self.domain_set_start or other.domain_set_start
+        self.index_shift = self.index_shift or other.index_shift
 
     def __iter__(self):
         return self.objs.__iter__()
+
+    def show_schedule(self):
+        if not self.schedule:
+            return ''
+        items = ', '.join('(%d, %d)' % (d, t) for d, t in self.schedule)
+        set_start = self.domain_set_start or 0
+        return '\ndomains {\n' \
+               '  schedule: [%s]\n' \
+               '  domain_set_start: %s\n' \
+               '  index_shift: %d\n' \
+               '}\n' % (items,
+                        'no_start' if set_start == -1 else str(set_start),
+                        self.index_shift or 0)
 
     def __repr__(self):
         return 'arch %(arch)s\n\n' \
                'objects {\n%(objs)s\n}\n\n' \
                'caps {\n%(caps)s\n}\n\n' \
-               'irq maps {\n%(irqs)s\n}' % {
+               'irq maps {\n%(irqs)s\n}\n' \
+               '%(domains)s' % {
 
                    # Architecture; arm11 or ia32
                    'arch': self.arch.capdl_name(),
@@ -62,4 +96,6 @@ class Spec(object):
                    'irqs': '\n'.join(sorted(
                        '%d: %s' % (x.number, x.name) for x in self.objs
                        if isinstance(x, IRQ) and x.number is not None)),
+
+                   'domains': self.show_schedule()
                }
